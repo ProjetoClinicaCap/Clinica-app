9fefd544917e82cd682867aa29ee11d5e4853c49
chayuna <chaozinhaa@gmail.com>
2022-06-28 11:22:56 -0300

    ajustes

--- clinica-app/src/app/consulta-usuario/consulta-usuario.component.ts
+++ clinica-app/src/app/consulta-usuario/consulta-usuario.component.ts
@@ -1,25 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from '../usuario.service';
 
-
 @Component({
   selector: 'app-consulta-usuario',
   templateUrl: './consulta-usuario.component.html',
-  styleUrls: ['./consulta-usuario.component.css']
+  styleUrls: ['./consulta-usuario.component.css'],
 })
 export class ConsultaUsuarioComponent implements OnInit {
-
   usuario: any;
 
-  constructor(private serviceUsuario: UsuarioService ) {
-    this.serviceUsuario.getAll().subscribe(x => this.usuario = x)
-   }
-
-  ngOnInit(): void {
+  constructor(private serviceUsuario: UsuarioService) {
+    this.serviceUsuario.getAll().subscribe((x) => (this.usuario = x));
   }
 
-  excluir(id:any){
-    this.serviceUsuario.excluir(id).subscribe(x => {window.location.reload()})
- }
+  ngOnInit(): void {}
 
+  excluir(id: any) {
+    this.serviceUsuario.excluir(id).subscribe((x) => {
+      window.location.reload();
+    });
+  }
 }
--- clinica-app/src/app/edita-usuario/edita-usuario.component.ts
+++ clinica-app/src/app/edita-usuario/edita-usuario.component.ts
@@ -5,27 +5,27 @@ import { UsuarioService } from '../usuario.service';
 @Component({
   selector: 'app-edita-usuario',
   templateUrl: './edita-usuario.component.html',
-  styleUrls: ['./edita-usuario.component.css']
+  styleUrls: ['./edita-usuario.component.css'],
 })
 export class EditaUsuarioComponent implements OnInit {
   user: any = {};
 
-  msg: string = "";
+  msg: string = '';
 
-  constructor(private route: ActivatedRoute,
-    private serviceUsuario: UsuarioService,) {
-    }
-
+  constructor(
+    private route: ActivatedRoute,
+    private serviceUsuario: UsuarioService
+  ) {}
 
   ngOnInit(): void {
     let routeParams = this.route.snapshot.paramMap;
-    let id: number = Number(routeParams.get('id'))
-    this.serviceUsuario.getOne(id).subscribe(x => this.user = x)
+    let id: number = Number(routeParams.get('id'));
+    this.serviceUsuario.getOne(id).subscribe((x) => (this.user = x));
   }
 
-  efetivarAlteracao(){
-    this.serviceUsuario.alterar(this.user).subscribe(x => this.msg = "Registro alterado com sucesso" )
+  efetivarAlteracao() {
+    this.serviceUsuario
+      .alterar(this.user)
+      .subscribe((x) => (this.msg = 'Registro alterado com sucesso'));
   }
-
-
 }
